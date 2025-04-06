bca85969a9c527ec37683c8b2e80aea3bc6d0065
icovv <hvasilev00@abv.bg>
2025-04-06 15:59:41 +0300

    adding id attribute to all profile page attributes

--- home-decor/src/components/profile/Profile.jsx
+++ home-decor/src/components/profile/Profile.jsx
@@ -1,6 +1,15 @@
+import useForm from "../../hooks/useForm"
 import styles from "./Profile.module.css"
 
 export default function Profile(){
+    let {} = useForm({
+      email:"",
+      name: "",
+      password: "",
+      town: "",
+
+    })
+
     return(
         <main>
   <div className={styles.container}>
@@ -14,31 +23,31 @@ export default function Profile(){
       <form>
         <div className={styles.section}>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input type="text" name="email" id="email" />
         </div>
         <div className={styles.section}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" id="name" />
         </div>
         <div className={styles.section}>
           <label htmlFor="password">Password</label>
-          <input type="text" name="password" />
+          <input type="text" name="password" id="password" />
         </div>
         <div className={styles.section}>
           <label htmlFor="town">Town</label>
-          <input type="text" name="town" />
+          <input type="text" name="town" id="town" />
         </div>
         <div className={styles.section}>
           <label htmlFor="streetName">Street Name</label>
-          <input type="text" name="streetName" />
+          <input type="text" name="streetName" id="streetName" />
         </div>
         <div className={styles.section}>
           <label htmlFor="streetNumber">Street Number</label>
-          <input type="number" name="streetNumber" />
+          <input type="number" name="streetNumber" id="streetNumber" />
         </div>
         <div className={styles.section}>
           <label htmlFor="tel">Phone Number</label>
-          <input type="tel" name="tel" />
+          <input type="tel" name="tel" id="tel"/>
         </div>
         <button type="submit" className={styles.button}>
           Save
