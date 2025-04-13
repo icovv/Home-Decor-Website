3e10d6301a1877e31de57a5676026aea61e875b7
icovv <hvasilev00@abv.bg>
2025-04-13 10:56:38 +0300

    adding error handling in the register submit handler

--- home-decor/src/hooks/useFormSubmitHandlers.jsx
+++ home-decor/src/hooks/useFormSubmitHandlers.jsx
@@ -13,7 +13,7 @@ export default function useFormSubmitHandlers(value, handler,changeValues,userID
 
         if(value.email.trim() == "" || value.password.trim() == ""){
             changeValues({email: value.email, password: ""});
-            console.log("All fields are required!"); // add error handling
+            setErr([{message:"All fields are required!"}])
             return;
         }
 
@@ -41,23 +41,23 @@ export default function useFormSubmitHandlers(value, handler,changeValues,userID
 
         console.log(value);
 
-        // if(value.email.trim() == "" || value.password.trim() == "" || value.repass.trim() == ""){
-        //     changeValues({email: value.email, password: "", repass: ""});
-        //     console.log("All fields are required!"); // add error handling
-        //     return;
-        // }
+        if(value.email.trim() == "" || value.password.trim() == "" || value.repass.trim() == ""){
+            changeValues({email: value.email, password: "", repass: ""});
+            setErr([{message:"All fields are required!"}])
+            return;
+        }
 
-        // if(value.password != value.repass){
-        //     changeValues({email: value.email, password: "", repass: ""});
-        //     console.log("Password must match!"); // add error handling
-        //     return;
-        // }
+        if(value.password != value.repass){
+            changeValues({email: value.email, password: "", repass: ""});
+            setErr([{message:"Password must match!"}]) // add error handling
+            return;
+        }
 
         let {email,password} = value;
 
         email = email.trim();
         password = password.trim();
-        console.log(password.length)
+
         let result = await handler(email,password);
 
         if(result.message){
