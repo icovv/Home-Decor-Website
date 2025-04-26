9c2eed684364c6237c36aeb8cbea354634392f39
icovv <hvasilev00@abv.bg>
2025-04-26 12:55:23 +0300

    adding get request for the Admin Serice

--- home-decor/src/api/AdminService.js
+++ home-decor/src/api/AdminService.js
@@ -1,7 +1,10 @@
-import { post } from "./requester";
+import { get, post } from "./requester";
 
 
 export async function adminCreateItem(type,formData){
     return await post(`http://localhost:3000/admin/${type}`,formData,false);
 }
 
+export async function getAdminItems(category) {
+    return await get(`http://localhost:3000/${category}`)
+}
