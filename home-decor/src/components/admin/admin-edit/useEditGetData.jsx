4c18cbc412d4d3b5af61848af6d4004a3f211fb6
icovv <hvasilev00@abv.bg>
2025-04-26 16:09:17 +0300

    creating custom hook handling initial info rendering for the Edit page

--- home-decor/src/components/admin/admin-edit/useEditGetData.jsx
+++ home-decor/src/components/admin/admin-edit/useEditGetData.jsx
@@ -1,9 +1,32 @@
-export default async function useEditGetData(id, changeValues) {
-    let getItemData = (e) => {
-        
-    }
+import { useEffect } from "react"
+import { adminGetSingleItem } from "../../../api/AdminService"
+import { useNavigate } from "react-router-dom"
 
-    return {
-        getItemData
-    }
+export default async function useEditGetData(id,category, changeValues,setImg) {
+    let navigate = useNavigate();
+    useEffect(() => {
+        let getItemData = async() => {
+            let categories = {"bedroom":"Bedroom", "decor":"Decor", "dining-room":"Dining-Room"}
+
+            let response = await adminGetSingleItem(category,id);
+            if(response.code == 404){
+                let err = [{message: response.message}]
+                navigate('*',{state: {errors: err} })
+                console.log(response.message);
+                return;
+            }
+
+            changeValues({
+                cat: categories[response.cat],
+                title: response.tittle,
+                col: response.col,
+                price: response.price,
+                description: response.description,
+                characteristics: response.characteristics
+            })
+
+            setImg(`data:${response.contentType};base64,${response.picture}`);
+        }
+        getItemData()
+    }, [])
 }
