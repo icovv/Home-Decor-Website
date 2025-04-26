8b70628e87f48c844db62d9df9c2bf48370620eb
icovv <hvasilev00@abv.bg>
2025-04-26 23:38:30 +0300

    importing auth context, cart context and custom useForm hook for the Order Details component in order to visualise user information and also the cart of the user

--- home-decor/src/components/cart/order/OrderDetails.jsx
+++ home-decor/src/components/cart/order/OrderDetails.jsx
@@ -1,6 +1,28 @@
+import { useContext } from "react"
 import styles from "./OrderDetails.module.css"
+import CartContext from "../../../contexts/CartContext"
+import OrderDetailsSingleItem from "./orderDetailsSIngleItem/OrderDetailsSIngleItem"
+import useForm from "../../../hooks/useForm"
+import AuthContext from "../../../contexts/AuthContext"
 
 export default function OrderDetails() {
+    let {state} = useContext(CartContext)
+    let {user} = useContext(AuthContext)
+    console.log(user)
+    let {value,changeHandler} = useForm({
+        fullName: user.name,
+        email: user.email,
+        phone: user.tel,
+        town: user.town,
+        streetNumber: user.streetNumber,
+        streetName: user.streetName,
+        comments:'',
+    });
+
+    let totalAmount = 0;
+
+    state.forEach(x => totalAmount += (x.counter * x.price))
+
     return (
         <main>
             <div className= {styles.container}>
@@ -13,29 +35,32 @@ export default function OrderDetails() {
                             <form>
                                 <div className={styles.data}>
                                     <h2>Personal Information</h2>
-                                    <input type="text" name="firstName" placeholder="First Name*" />
-                                    <input type="text" name="lastName" placeholder="Last Name*" />
-                                    <input type="text" name="email" placeholder="Email*" />
-                                    <input type="text" name="phone" placeholder="Phone*" />
+                                    <input type="text" name="fullName" id="fullName" placeholder="Full Name*" value={value.fullName || ""} onChange={changeHandler}/>
+                                    <input type="text" name="email" id="email" placeholder="Email*" value={value.email || ""} onChange={changeHandler} />
+                                    <input type="text" name="phone" id="phone" placeholder="Phone*" value={value.phone || ""} onChange={changeHandler} />
                                 </div>
                                 <div className={styles.data}>
                                     <h2>Delivery Address</h2>
                                     <div className={styles["city-postal"]}>
-                                        <input type="text" name="city" placeholder="City*" />
+                                        <input type="text" name="town" id="town" placeholder="Town*" value={value.town || ""} onChange={changeHandler} />
                                         <input
                                             type="number"
-                                            name="postalCode"
-                                            placeholder="Postal Code*"
+                                            name="streetNumber"
+                                            placeholder="Street Number*"
+                                            id="streetNumber"
+                                            value={value.streetNumber || ""} onChange={changeHandler}
                                         />
                                     </div>
-                                    <input type="text" name="addess" placeholder="Address*" />
+                                    <input type="text" name="streetName" id="streetName" placeholder="Street Name*" value={value.streetName || ""} onChange={changeHandler} />
                                 </div>
                                 <div className={styles.data}>
                                     <h2>Comments</h2>
                                     <textarea
                                         type="text"
                                         placeholder="Additional Comments*"
-                                        defaultValue={""}
+                                        id="comments"
+                                        name="comments"
+                                        value={value.comments || ""} onChange={changeHandler} 
                                     />
                                 </div>
                             </form>
@@ -44,25 +69,16 @@ export default function OrderDetails() {
                     <div className={styles["right-side"]}>
                         <h3>Your Order</h3>
                         <div className={styles.container}>
-                            <div className={styles["single-item"]}>
-                                <div className={styles["left-side"]}>
-                                    <img
-                                        src="/bedroom.jpg"
-                                        alt=""
-                                    />
-                                </div>
-                                <div className={styles["right-side"]}>
-                                    <h3 className={styles["text-center"]}>Product Name</h3>
-                                    <div className={styles["price-quantity"]}>
-                                        <p>10 x 65.00$</p>
-                                        <p>65.00$</p>
-                                    </div>
-                                </div>
-                            </div>
+                            {state.length > 0
+                            ?
+                            state.map(x => <OrderDetailsSingleItem key={x.itemId} item={x}></OrderDetailsSingleItem>)
+                            
+                            :
+                            <></>}
                         </div>
                         <div className={styles["total-amount"]}>
                             <h4>Total Amount</h4>
-                            <p>200.00$</p>
+                            <p>{totalAmount} $</p>
                         </div>
                         <a>Continue</a>
                     </div>
