f5144229b2752675d2ffcfe4963c8746cf3b29cf
icovv <hvasilev00@abv.bg>
2025-04-26 12:56:12 +0300

    adding custom hook to the component, implementing error handling

--- home-decor/src/components/admin/admin-list/AdminList.jsx
+++ home-decor/src/components/admin/admin-list/AdminList.jsx
@@ -1,73 +1,90 @@
 import { Link } from "react-router-dom"
 import styles from "./AdminList.module.css"
 import AdminSingleItem from "./admin-single-item/AdminSingleItem"
-import { useEffect, useState } from "react"
+import useGetItemsData from "./useGetItemsData";
+import Error from "../../common/errors/Error";
 
 export default function AdminList() {
 
-    let [Items,setItems] = useState(null);
-    let [image,setImg] = useState(null);
-    useEffect(() => {
-        async function getData() {
-            try {
-                let response = await fetch('http://localhost:3000/bedroom',{
-                    method:"GET"
-                });
-                let result = await response.json();
-                setItems(result.items)
-                
-                setImg(`data:${result.items[0].contentType};base64,${result.items[0].picture}`)
-            } catch (error) {
-                console.log(error)
-            }
-
-        }
-        getData()
-    },[])
+    let {data,useGetBedroomItems, useGetDecorItems, useGetDiningRoomItems,err,divKill} = useGetItemsData()
 
     return (
         <main>
+            {err.length > 0
+            ?
+            <Error err={err} divKill={divKill}></Error>
+            :
+            <></>
+            }
+
         <div className={styles.menu}>
             <ul>
                 <Link to='/admin/list' className={`${styles.item} ${styles.underline} ${styles.hover}`}>Items</Link>
                 <Link to='/admin/create' className={`${styles.item} ${styles.hover}`}>Create</Link>
             </ul>
         </div>
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles["change-page"]}>
-                    <ul>
-                        <li>
-                            <i className="fa-solid fa-arrow-left" />
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-arrow-right" />
-                        </li>
-                    </ul>
-                </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Collection</th>
-                            <th>Price</th>
-                            <th>Description</th>
-                            <th>Characteristics</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Items? Items.length > 0 ?
-                        Items.map(item => <AdminSingleItem key={item._id} item={item}></AdminSingleItem>)
-                        :
-                        <></>
-                        :
-                        <></>}
-                    </tbody>
-                </table>
-            </div>
+        <div className={`${styles.menu} ${styles['second-ul']}`}>
+            <ul>
+                <li className={`${styles.item} ${styles.hover}`} id="bedroom" onClick={useGetBedroomItems}>Bedroom</li>
+                <li className={`${styles.item} ${styles.hover}`} id="decor"  onClick={useGetDecorItems}>Decor</li>
+                <li className={`${styles.item} ${styles.hover}`} id="dining-room"  onClick={useGetDiningRoomItems}>Dining Room</li>
+            </ul>
         </div>
+        {data.length > 0 
+        ?
+        <div className={styles.container}>
+        <div className={styles.content}>
+            <div className={styles["change-page"]}>
+                <ul>
+                    <li>
+                        <i className="fa-solid fa-arrow-left" />
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-arrow-right" />
+                    </li>
+                </ul>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Collection</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Characteristics</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                    data.map(item => <AdminSingleItem key={item._id} item={item}></AdminSingleItem>)
+                    }
+                </tbody>
+            </table>
+        </div>
+        </div>
+        :
+        <div className={styles.container}>
+        <div className={styles.content}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Collection</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Characteristics</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+        </div>}
         </main>
     )
 }
