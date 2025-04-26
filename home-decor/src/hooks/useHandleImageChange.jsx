40f2196a3c12b0b486c858fa6b4584be181169a0
icovv <hvasilev00@abv.bg>
2025-04-26 19:49:26 +0300

    adding validation to the image handler.

--- home-decor/src/hooks/useHandleImageChange.jsx
+++ home-decor/src/hooks/useHandleImageChange.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 export default function useHandleImageChange(setImgFile,setImg){
+    let [error,setErr] = useState([]);
+
     let handleImageChange = (e) => {
         //add validation for image type (extension - jpg/png etc )
         let file = e.target.files[0];
         if(file){
+            let fileTypes = ["image/jpeg", "image/png", "image/gif", "image/jpg"];
+            if(!fileTypes.includes(file.type)){
+                setErr([{message: "The image supports only jpeg, png, gif and jpg file types!"}])
+                return;
+            }
             setImgFile(file);
             let reader = new FileReader();
             reader.onloadend = () => {
@@ -13,7 +22,13 @@ export default function useHandleImageChange(setImgFile,setImg){
 
     }
 
+    let killDiv = () => {
+        setErr([]);
+    }
+
     return {
+        error,
+        killDiv,
         handleImageChange
     }
 }
